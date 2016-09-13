d3bf9dd7b8d4555b6cb3ff69240049a0f12d4678
Kelvin Lin <kelvin@backer-founder.com>
2016-09-13 18:00:30 +0800

    init

---
+++ public/assets/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $(".number-20").animateNumbers(20, true, 2800);
+  $(".number-100").animateNumbers(100, true, 3800);
+
+});
+
+//reveal效果
+window.sr = ScrollReveal();
+sr.reveal('.sec1 .reveal',300);
+sr.reveal('.sec2 .reveal',400);
+sr.reveal('.sec3 .reveal',400);
+sr.reveal('.sec4 .reveal',400);
+sr.reveal('.sec5 .reveal',340);
+
+
+
+
