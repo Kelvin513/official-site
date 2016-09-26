e18f11a8c01543646875c6aaf57226cde3325e95
bunnieabc <bunnieabc@gmail.com>
2016-09-26 18:30:37 +0800

    about.html finish

--- public/assets/js/about.js
+++ public/assets/js/about.js
@@ -10,7 +10,52 @@ $(document).ready(function() {
     $(".modal-wrap").fadeOut(300);
     //$(".modal").fadeOut(300);
   })
-  
+  $("button.close").click(function(){
+    $(".modal-wrap").fadeOut(300);
+    //$(".modal").fadeOut(300);
+  })
+  $(".tab-unit.intro").click(function(){
+    $(".modal-case").css("display","none");
+    $(".modal-intro").fadeIn(300);
+    $(".intro").addClass("active");
+    $(".case").removeClass("active");
+  })
+  $(".tab-unit.case").click(function(){
+    $(".modal-intro").css("display","none");
+    $(".modal-case").fadeIn(300);
+    $(this).addClass("active");
+    $(".intro").removeClass("active");
+  })
+  $(".previous").click(function(){
+    //$(".modal-content").css("display","none");
+    
+    setTimeout(function(){ 
+      $(".modal-content").fadeOut(100);
+      //$(".modal-content").css("display","none");
+      setTimeout(function(){ 
+        $(".modal-content").fadeIn(400);
+        $(".modal-title").html("Web Developer<br> 貝殼杯麵");
+        $(".modal-content-block.right").css("background-image","url('/assets/img/main/test.png')");
+        $(".modal-intro").html("貝殼杯麵貝殼杯麵貝殼杯麵超胖超讚")
+        $(".modal-name").html("<h3>Baymax <span>杯麵呀</span></h3>")
+      }, 400);
+    },100);
+    
+  })
+  $(".next").click(function(){
+     setTimeout(function(){ 
+      $(".modal-content").fadeOut(100);
+      //$(".modal-content").css("display","none");
+      setTimeout(function(){ 
+        $(".modal-content").fadeIn(400);
+        $(".modal-title").html("Web Developer<br> 挖洗杯麵唷");
+        $(".modal-name").html("<h3>Fat Uncle <span>你的胖叔</span></h3>")
+        $(".modal-content-block.right").css("background-image","url('/assets/img/main/test.jpg')");
+        $(".modal-intro").html("我是比較邪惡的杯麵，我是比較邪惡的杯麵我是比較邪惡的杯麵我是比較邪惡的杯麵")
+      }, 400);
+    },100);
+  })
+
 });
 
 //reveal效果
---
+++ public/assets/js/test.js
@@ -0,0 +1,20 @@
+var test=[
+{
+  "id":0,
+  "name":"杯麵HIHI",
+  "title":"Web Developer",
+  "img":"/assets/img/main/test.jpg"
+},
+{
+  "id":1,
+  "name":"nana",
+  "title":"Web Developer",
+  "img":"/assets/img/main/nana.png"
+},
+{
+  "id":2,
+  "name":"杯麵仔",
+  "title":"Web Developer",
+  "img":"/assets/img/main/test.png"
+}
+]
